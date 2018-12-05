017c0d4e8edd6bb298f715ca98bb9752f822766c
Ida <ida.fagerlund@hotmail.com>
2018-12-05 20:12:38 +0100

    Finished the AddContact class enough for it to work.

--- src/Database/AddContact.java
+++ src/Database/AddContact.java
@@ -1,4 +1,22 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AddContact {
+
+    public void addContact(String name, String number) {
+        String sql = "INSERT INTO phone_book(name,number) VALUES(?,?)";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/phone_book.db");
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, number);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
