9a64189e3e1485349606896f5b8b351ccf968d9e
Ida <ida.fagerlund@hotmail.com>
2018-12-05 19:59:16 +0100

    Finished the CreateDatabase class enough for it to work.

--- src/Database/CreateDatabase.java
+++ src/Database/CreateDatabase.java
@@ -1,4 +1,29 @@
 package Database;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateDatabase {
+
+    public void createDatabaseIfNotExist(){
+
+        String sql = "CREATE TABLE phone_book (\n"
+                + " contact_id INTEGER PRIMARY KEY,\n"
+                + "	name TEXT NOT NULL CHECK (length(name) < 25),\n"
+                + "	number TEXT NOT NULL CHECK (length(number) > 8 AND length(number) < 15)\n"
+                + ");";
+
+        if(!new File("C:/sqlite/db/phone_book.db").isFile()){
+            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/phone_book.db")) {
+                Statement stmt = conn.createStatement();
+                stmt.execute(sql);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
