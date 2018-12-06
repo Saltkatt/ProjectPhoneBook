18e94de6492f222ae8a5de03ff32668763ccb82e
Daniella Noren <daniellanoren@live.se>
2018-12-06 16:27:19 +0100

    Add readMenu()-method that is called from all different menu-methods.

--- src/main/java/userinteraction/Menu.java
+++ src/main/java/userinteraction/Menu.java
@@ -30,15 +30,32 @@ public class Menu {
     private void fillMainMenu(){
         mainMenuList = new ArrayList<>();
        // mainMenuList.add(new MenuOptions("1. Add conctact", cm.addContact()))
-       // mainMenuList.add(new MenuOptions("2. Edit contact",
+       // mainMenuList.add(new MenuOptions("2. Edit contact", () -> editMenu());
 
     }
 
     private void fillEditMenu(){
         editMenuList = new ArrayList<>();
-
+        //editMenuList.add(new MenuOptions("1. Search for contact, () -> cm.select(UserInput.getString())); This could also go to another menu. There should be a method for retrieving name/phonenumber?
+        //editMenuList.add(new MenuOptions("2. Delete contact, () -> cm
+        //editMenuList.add(new MenuOptions("3. Go back", () -> mainMenu());
 
     }
+
+    private void readMenu(ArrayList<MenuOptions> list){
+        //TODO: Call useroutput to print each String
+        //int option = in.getOption(list); TODO: Get int-answer from userInput
+        //list.get(Integer.parseInt(option)-1).getDoIt().doThing(); Run method
+    }
+
+    public void mainMenu(){
+       readMenu(mainMenuList);
+    }
+
+    public void editMenu(){
+        readMenu(editMenuList);
+    }
+
 
 
 }
