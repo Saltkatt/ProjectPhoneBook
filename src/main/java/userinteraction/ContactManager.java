ef8b2b41466a0529224af2e561fc997b24eed7db
anton <anton.blixter@gmail..com>
2018-12-06 22:36:24 +0100

    Add createContact method

--- src/main/java/userinteraction/ContactManager.java
+++ src/main/java/userinteraction/ContactManager.java
@@ -6,5 +6,11 @@ public class ContactManager {
     This class manages the user interactions which concern creating new and editing existing contacts.
      */
 
+    /*
+    Getting info from user for creating a contact and providing the result to database communication method.
+     */
+    public void createContect() {
+        addContact.addContact(enterName(), enterPhoneNumber());
+    }
 
 }
