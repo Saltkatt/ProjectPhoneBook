dae92888f2bceb7645d3481a699047f57ad7daf0
Daniella Noren <daniellanoren@live.se>
2018-12-06 15:37:47 +0100

    Add ArrayLists of MenuOptions to Menu and methods to fill them

--- src/main/java/userinteraction/Menu.java
+++ src/main/java/userinteraction/Menu.java
@@ -1,5 +1,8 @@
 package userinteraction;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * This class creates menus
  *
@@ -12,10 +15,24 @@ public class Menu {
     private ContactManager cm;
     private UserInput in;
     private UserOutput out;
+    private ArrayList<MenuOptions> mainMenuList;
+    private ArrayList<MenuOptions> editMenuList;
 
     public Menu(ContactManager cm, UserOutput out, UserInput in){
         this.cm = cm;
         this.in = in;
         this.out = out;
+    }
+
+    private final ArrayList<MenuOptions> fillMainMenu(){
+        mainMenuList = new ArrayList<>();
+
+        return mainMenuList;
+    }
+
+    private final ArrayList<MenuOptions> fillEditMenu(){
+        editMenuList = new ArrayList<>();
+
+        return editMenuList;
     }
 }
