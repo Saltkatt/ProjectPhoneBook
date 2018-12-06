2a63d79e3769b6975fdfd1231f320ea65abc94f3
Daniella Noren <daniellanoren@live.se>
2018-12-06 17:45:46 +0100

    Add Junit-tests for Menu and MenuOptions

---
+++ src/test/java/userinteraction/MenuTest.java
@@ -0,0 +1,14 @@
+package userinteraction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuTest {
+
+    @org.junit.jupiter.api.Test
+    void mainMenu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void editMenu() {
+    }
+}
