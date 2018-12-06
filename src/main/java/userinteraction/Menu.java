369302270bb7bb0ccca6dea7ec7299615f85b7af
Daniella Noren <daniellanoren@live.se>
2018-12-06 15:01:30 +0100

    Add constructor to Menu

--- src/main/java/userinteraction/Menu.java
+++ src/main/java/userinteraction/Menu.java
@@ -6,4 +6,9 @@ public class Menu {
     private UserInput in;
     private UserOutput out;
 
+    public Menu(ContactManager cm, UserOutput out, UserInput in){
+        this.cm = cm;
+        this.in = in;
+        this.out = out;
+    }
 }
