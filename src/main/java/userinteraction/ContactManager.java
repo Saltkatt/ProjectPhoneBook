d8ded3c42c68830c73094d2268abce15a77a1a15
anton <anton.blixter@gmail..com>
2018-12-06 21:58:02 +0100

    Add class description comment

--- src/main/java/userinteraction/ContactManager.java
+++ src/main/java/userinteraction/ContactManager.java
@@ -1,4 +1,10 @@
-package userinteraction;
+package main.java.userinteraction;
 
 public class ContactManager {
+
+    /*
+    This class manages the user interactions which concern creating new and editing existing contacts.
+     */
+
+
 }
