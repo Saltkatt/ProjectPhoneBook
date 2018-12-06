dc2a899e1fe08ddf5e7f34340443b5100a7f97dd
anton <anton.blixter@gmail..com>
2018-12-06 23:19:45 +0100

    add name user input method

--- src/main/java/userinteraction/UserInput.java
+++ src/main/java/userinteraction/UserInput.java
@@ -1,4 +1,31 @@
-package userinteraction;
+package main.java.userinteraction;
+
+import java.util.Scanner;
 
 public class UserInput {
+
+    /**
+     This class handles all user output.
+     */
+
+    private static Scanner sc = new Scanner(System.in);
+
+    /**
+     Controls the user input and returns it when it's a valid name input.
+     */
+    public static String name() {
+        String name;
+        boolean validInput;
+        do {
+            name = sc.nextLine().trim();
+            if (name.matches("[a-öA-Ö\\-\\s\']{1,30}")) {
+                validInput = true;
+            } else {
+                UserOutput.printLine("Invalid input, try again!");
+                validInput = false;
+            }
+        } while (!validInput);
+        return name;
+    }
+
 }
