d6a7e4c420a471bda80403c0827f2a1fdedd4b50
Elin <elin.sexton@iths.se>
2018-12-07 10:44:06 +0100

    Added a selectContact method with 3 String variables with SQL statements. Method unfinished. Added an updateContact metod with 2 String variables with SQL statements. Method unfinished.

--- src/main/java/database/SelectContact.java
+++ src/main/java/database/SelectContact.java
@@ -1,5 +1,25 @@
 package database;
 
+/**
+ * This class selects contacts from the database
+ * @author Elin Sexton
+ */
+
 public class SelectContact {
+
+    /**
+     * Selects a contact from the database
+     * @param name of the contact
+     * @param number of the contact
+     */
+
+    public void selectContact(String name, String number) {
+
+        String selectNameSQL = "SELECT " + name + " FROM phone_book;";
+        String selectNumberSQL = "SELECT " + number + " FROM phone_book;";
+        String selectAllSQL = "SELECT * FROM phone_book;";
+
+    }
+
 
 }
--- src/main/java/database/UpdateContact.java
+++ src/main/java/database/UpdateContact.java
@@ -1,4 +1,18 @@
 package database;
 
 public class UpdateContact {
+
+    /**
+     * Updates a selected statement.
+     * @param name of the contact.
+     * @param number of the contact.
+     */
+
+    public void updateStatement(String name, String number) {
+
+        String updateNameSQL = "UPDATE name FROM phone_book WHERE name=" + name + ";";
+        String updateNumberSQL = "UPDATE number FROM phone_book WHERE number=" + number + ";";
+
+    }
+
 }
