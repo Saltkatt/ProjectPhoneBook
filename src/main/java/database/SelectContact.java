b857c2c6cb9ce13fa224b099ae40c42cbb2fd242
Elin <elin.sexton@iths.se>
2018-12-08 17:42:29 +0100

    Added a selectNameContact method that selects contacts based on the name received and prints those contacts with that name.

--- src/main/java/database/SelectContact.java
+++ src/main/java/database/SelectContact.java
@@ -32,5 +32,29 @@ public class SelectContact {
         }
 
     }
-    
+
+    /**
+     * Selects a contact based on the input name.
+     * @param name of the contact.
+     */
+    public void selectNameContact(String name){
+
+        String selectName = "SELECT contact_id, name, number FROM phone_book WHERE name= ?";
+
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/phone_book.db");
+             PreparedStatement pstmt  = con.prepareStatement(selectName)){
+            pstmt.setString(1,name);
+            ResultSet rs  = pstmt.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("contact_id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getString("number"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
