4d2420b5ed19728ffd6910dfb59714f618d0c60b
Ida <ida.fagerlund@hotmail.com>
2018-12-08 10:51:58 +0100

    Changing the savepath for the database so it saves in the same directory as the project. Also fixed issues so the code now creates the directory if it doesn't exist already and is platform independent.

--- src/main/java/database/CreateDatabase.java
+++ src/main/java/database/CreateDatabase.java
@@ -13,24 +13,27 @@ import java.sql.Statement;
 public class CreateDatabase {
 
     /**
-     * Checks if the database exists. If it doesn't, it creates it.
+     * Creates a database in the default project path. If the user have a different path than the default,
+     * it creates the directories before creating the databasefile. If it needs to create a path it's platform independent.
      */
     public void createDatabaseIfNotExist(){
 
-        String sql = "CREATE TABLE phone_book (\n"
+        String sql = "CREATE TABLE IF NOT EXISTS phone_book (\n"
                 + " contact_id INTEGER PRIMARY KEY,\n"
                 + "	name TEXT NOT NULL CHECK (length(name) < 25),\n"
                 + "	number TEXT NOT NULL CHECK (length(number) > 8 AND length(number) < 15)\n"
                 + ");";
 
 
-        if(!new File("C:/sqlite/db/phone_book.db").isFile()){
-            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/phone_book.db")) {
-                Statement stmt = conn.createStatement();
-                stmt.execute(sql);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        String saveDir = System.getProperty("user.home") + File.separator + "IdeaProjects" + File.separator +
+                "ProjectPhoneBook" + File.separator;
+        new File(saveDir).mkdirs();
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + saveDir + "phone_book.db")) {
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
