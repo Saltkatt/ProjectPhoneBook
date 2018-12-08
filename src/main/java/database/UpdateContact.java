9206cf24a0b195791ed1b1d66feb8550ecd4bdd5
Elin <elin.sexton@iths.se>
2018-12-08 16:51:30 +0100

    Added database connection for name update.

--- src/main/java/database/UpdateContact.java
+++ src/main/java/database/UpdateContact.java
@@ -1,5 +1,10 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UpdateContact {
 
     /**
@@ -12,6 +17,20 @@ public class UpdateContact {
 
         String updateName = "UPDATE phone_book SET name=" +name+ "WHERE contact_id=" +contact_id;
         String updateNumber = "UPDATE phone_book SET number=" +number+ "WHERE contact_id=" +contact_id;
+
+
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/db/phone_book.db");
+             PreparedStatement pstmt = con.prepareStatement(updateName)) {
+            pstmt.setString(1, name);
+            pstmt.setInt(2, contact_id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
 
 
 
