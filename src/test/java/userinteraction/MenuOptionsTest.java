7ac7f12204f767eb601d9dfbdee327b2891996f9
Daniella Noren <daniellanoren@live.se>
2018-12-10 22:10:42 +0100

    Finish getDoIt- and badStringTest in MenuOptionsTest

--- src/test/java/userinteraction/MenuOptionsTest.java
+++ src/test/java/userinteraction/MenuOptionsTest.java
@@ -12,8 +12,19 @@ class MenuOptionsTest {
 
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getDoIt() {
+        DoSomething d = () -> System.out.println("Test successful");
+        DoSomething e = () -> System.out.println("Test");
+        MenuOptions mo = new MenuOptions("1", d);
+        assertEquals(d, mo.getDoIt());
+        assertFalse(mo.getDoIt().equals(e));
+        assertTrue(!(mo.getDoIt().equals(e)));
+        assertTrue(mo.getDoIt().equals(d));
+        MenuOptions mo2 = new MenuOptions("2", e);
+        assertFalse(mo2.getDoIt().equals(d));
+        assertTrue(mo2.getDoIt().equals(e));
+        assertFalse(mo2.getDoIt().equals(mo.getDoIt()));
     }
 
     @org.junit.jupiter.api.Test
