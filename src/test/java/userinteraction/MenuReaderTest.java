2b67c0c9547bc16a64fa3d069a62a7755d28e96c
Daniella Noren <daniellanoren@live.se>
2018-12-11 17:27:50 +0100

    Add to setUp-method, add arrayList and String fields in MenuReaderTest

--- src/test/java/userinteraction/MenuReaderTest.java
+++ src/test/java/userinteraction/MenuReaderTest.java
@@ -4,17 +4,25 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MenuReaderTest {
 
+    private String test;
+    ArrayList<MenuOptions> mo = new ArrayList<>();
+
     @BeforeAll
     void setUp(){
-
+        mo.add(new MenuOptions("1. Say hello", () -> System.out.println("Hello")));
+        mo.add(new MenuOptions("2. Change Stringtest to \"Hello\"", () -> test = "Hello"));
     }
 
     @Test
     void printMenu() {
+
+        assertEquals("1. Say hello\n2. Change Stringtest to \"Hello\"", MenuReader.printMenu(mo));
     }
 
     @Test
