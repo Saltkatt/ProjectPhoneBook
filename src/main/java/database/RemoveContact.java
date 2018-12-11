973b2d11621faa625f2d8758a131a1ac2b5bd345
Ida <ida.fagerlund@hotmail.com>
2018-12-11 13:00:02 +0100

    minor fixes

--- src/main/java/database/RemoveContact.java
+++ src/main/java/database/RemoveContact.java
@@ -15,12 +15,19 @@ public class RemoveContact {
      * Deletes an existing contact from the database.
      * @param contact_id of the contact.
      */
+
+    private String fileName;
+
+    public RemoveContact(String fileName){
+        this.fileName = fileName;
+    }
+
     public void removeContact(int contact_id){
         //String SQL-statement to delete contact.
         String removeSQL = "DELETE FROM contacts WHERE contact_id= ?";
 
         //Connects to the database and deletes received contact_id.
-        try (Connection con = DriverManager.getConnection("jdbc:sqlite:phone_book.db");
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:" + fileName);
              PreparedStatement pstmt = con.prepareStatement(removeSQL)) {
             pstmt.setInt(1, contact_id);
             pstmt.executeUpdate();
