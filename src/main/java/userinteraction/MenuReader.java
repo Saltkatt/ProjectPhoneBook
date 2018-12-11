4493543bf2decaa55b4a060f30ee5fa88d61603f
Daniella Noren <daniellanoren@live.se>
2018-12-11 16:19:19 +0100

    Add methods printMenu and executeMenu to MenuReader

--- src/main/java/userinteraction/MenuReader.java
+++ src/main/java/userinteraction/MenuReader.java
@@ -1,4 +1,17 @@
 package userinteraction;
 
+import java.util.ArrayList;
+
 public class MenuReader {
+
+    public void printMenu(ArrayList<MenuOptions> list){
+        if(list.size() > 0){
+            list.forEach(e -> UserOutput.printLine(e.getMenuText()));
+        }
+    }
+    public void executeMenu(ArrayList<MenuOptions> list, int choice){
+        list.get(choice).getDoIt().doThing();
+    }
+
+
 }
