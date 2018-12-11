e55f1fe523b48a593059806ba153ab4614973e9b
Daniella Noren <daniellanoren@live.se>
2018-12-11 17:03:18 +0100

    Add a setup-method

--- src/test/java/userinteraction/MenuReaderTest.java
+++ src/test/java/userinteraction/MenuReaderTest.java
@@ -1,10 +1,16 @@
 package userinteraction;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuReaderTest {
+
+    @BeforeAll
+    void setUp(){
+
+    }
 
     @Test
     void printMenu() {
