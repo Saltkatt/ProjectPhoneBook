a1ae9640497c176cfb3fe96b46a3ba39390015c2
Daniella Noren <daniellanoren@live.se>
2018-12-11 18:27:49 +0100

    Finish and rename executeMenuTest in MenuReaderTest

--- src/test/java/userinteraction/MenuReaderTest.java
+++ src/test/java/userinteraction/MenuReaderTest.java
@@ -12,12 +12,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MenuReaderTest {
 
-    private String test;
+    private String test = "";
     ArrayList<MenuOptions> mo = new ArrayList<>();
 
     @BeforeAll
     void setUp(){
-        mo.add(new MenuOptions("1. Say hello", () -> System.out.println("Hello")));
+        mo.add(new MenuOptions("1. Say hello", () -> System.out.print("Hello")));
         mo.add(new MenuOptions("2. Change Stringtest to \"Hello\"", () -> test = "Hello"));
     }
 
@@ -37,10 +37,17 @@ class MenuReaderTest {
     }
 
     @Test
-    void executeMenu() {
+    void executeMenuTest() {
+        PrintStream stream = new PrintStream(System.out);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        mo.get(0).getDoIt().doThing();
+        assertEquals("Hello", out.toString());
+        System.setOut(stream);
+        assertEquals("", test);
+        mo.get(1).getDoIt().doThing();
+        assertEquals("Hello", test);
+
     }
 
-    @Test
-    void readAndExecuteMenu() {
-    }
 }
