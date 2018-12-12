624dfbaae8759219468ef5c57db076223b965cfd
Ida <ida.fagerlund@hotmail.com>
2018-12-12 19:28:23 +0100

    Writing more tests for AddContact class and updating the database to fix a small bug

--- src/test/java/database/AddContactTest.java
+++ src/test/java/database/AddContactTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,7 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AddContactTest {
 
-    Database db;
+    
+    private Database db;
 
     @BeforeAll
     void setupDatabase(){
@@ -52,49 +52,26 @@ class AddContactTest {
             fail(e.getMessage());
         }
     }
-//
-//    
-//
-//    @Test
-//    void testThatCharactersCantExceedMaxLimit(){
-//        Database db = new Database("insert_test5.db");
-//
-//        db.addContact.addContact("aaaaaaaaaaaaaaaaaaaaaaaaa", "1111111111111111111111111111");
-//        db.addContact.addContact("aaaaaaaaaaaaaaaaaaaaaaaa", "111111111111111111111111111111");
-//        db.addContact.addContact("aaaaaaaaaaaaaaaaaaaaaaaaa", "111111111111111111111111111");
-//        db.addContact.addContact("aaaaaaaaaaaaaaaaaaaaaaaa", "1111111111111111111111111111");
-//
-//        assertEquals(1, db.selectContact.selectAllContact().size());
-//
-//        try {
-//            Files.deleteIfExists(Paths.get("insert_test5.db"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void testThatPhoneNumberOnlyCanContainNumbers(){
-//        Database db = new Database("insert_test6.db");
-//
-//        try {
-//            Files.deleteIfExists(Paths.get("insert_test6.db"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
+
+    @Test
+    void testThatCharactersCantExceedMaxLimit(){
+        try{
+            db.addContact.addContact("aaaaaaaaaaaaaaaaaaaaaaaaa", "11111111111111111111");
+        } catch(SQLException e){
+            fail(e.getMessage());
+        }
+        assertThrows(SQLException.class, () ->
+                db.addContact.addContact("aaaaaaaaaaaaaaaaaaaaaaaaaa", "111111111111111111111"));
+    }
 
     @AfterAll
-    void removeDatabaseSoTheTestAlwaysRunWithANewOne(){
+    void removeDatabaseSoTheTestAlwaysRunsWithANewOne(){
         try {
             Files.deleteIfExists(Paths.get("database_test_insert.db"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
-
 
 }
