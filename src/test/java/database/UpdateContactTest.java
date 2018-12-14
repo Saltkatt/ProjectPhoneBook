a8819af1bd3da9f148e5523a3a222f975d4f5e21
Elin <elin.sexton@iths.se>
2018-12-14 10:42:33 +0100

    Created a testing database for updates.

--- src/test/java/database/UpdateContactTest.java
+++ src/test/java/database/UpdateContactTest.java
@@ -1,8 +1,16 @@
 package database;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class UpdateContactTest {
+
+    private Database db;
+    @BeforeAll
+    void setupDatabase() {
+        db = new Database("database_testing_update");
+        
+    }
 
     /**
      * Tests for updating name and number.
