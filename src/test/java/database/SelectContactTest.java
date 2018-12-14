439075ed8005b19e8e8e998606d8908e6a6518c6
Elin <elin.sexton@iths.se>
2018-12-14 11:49:46 +0100

    Added a method to remove the testing database in SelectContactTest. adjusted the name of testing database.

--- src/test/java/database/SelectContactTest.java
+++ src/test/java/database/SelectContactTest.java
@@ -1,9 +1,13 @@
 package database;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SelectContactTest {
@@ -11,7 +15,7 @@ class SelectContactTest {
     private Database db;
     @BeforeAll
     void setupDatabase() {
-        db = new Database("database_testing_update");
+        db = new Database("database_testing_select");
         //database to check updates against.
         try {
             db.getAddContact().addContact("Anton", "0107433221");
@@ -42,6 +46,16 @@ class SelectContactTest {
     @Test
     void testSelectNumber() {
 
+    }
+
+    //removes testing database after the tests.
+    @AfterAll
+    void removeDatabase(){
+        try {
+            Files.deleteIfExists(Paths.get("database_testing_select"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
