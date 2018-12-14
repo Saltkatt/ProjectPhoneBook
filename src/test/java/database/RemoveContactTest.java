529eea9d9642ed85804c02a12b2077b41e3aba7a
Elin <elin.sexton@iths.se>
2018-12-14 12:06:41 +0100

    Added testing database for RemoveContactTest

--- src/test/java/database/RemoveContactTest.java
+++ src/test/java/database/RemoveContactTest.java
@@ -1,8 +1,28 @@
 package database;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 class RemoveContactTest {
+
+    private Database db;
+    @BeforeAll
+    void setupDatabase() {
+        db = new Database("database_testing_remove");
+        //database to check updates against.
+        try {
+            db.getAddContact().addContact("Anton", "0107433221");
+            db.getAddContact().addContact("Bertil", "0206122334");
+            db.getAddContact().addContact("Carl", "0305564738");
+            db.getAddContact().addContact("Daniella", "0404659301");
+            db.getAddContact().addContact("Elin", "0509384929");
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     /**
      * Test removeContact.
