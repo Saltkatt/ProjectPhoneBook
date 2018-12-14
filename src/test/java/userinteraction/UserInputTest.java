476002ede797e7deaa426de0ce975c7550b7d5f5
anton <anton.blixter@gmail..com>
2018-12-14 13:13:57 +0100

    Complete list choice tests

--- src/test/java/userinteraction/UserInputTest.java
+++ src/test/java/userinteraction/UserInputTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -63,6 +66,46 @@ class UserInputTest {
             if (shouldBeValid && returnedInput.equals(userInput)) {
                 assertTrue(true);
             } else if (shouldBeValid && !returnedInput.equals(userInput)) {
+                fail("User input should be valid and is returned as a valid input, but is not returned correctly");
+            } else {
+                fail("User input should not be valid but is returned as a valid input");
+            }
+        } catch (Exception e) {
+            String actual = out.toString().trim().replace("\r", "");
+            String expected = "Invalid input, try again!";
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void checkValidListChoiceInput() {
+        List<String> testList = Arrays.asList("This", "Is", "A", "Test", "List");
+        checkIfUserInputIsValidOrNot(testList, true, "1");
+        checkIfUserInputIsValidOrNot(testList, true, "5");
+    }
+
+    @Test
+    void checkFaultyListChoiceInput() {
+        List<String> testList = Arrays.asList("This", "Is", "A", "Test", "List");
+        checkIfUserInputIsValidOrNot(testList, false, " ");
+        checkIfUserInputIsValidOrNot(testList, false, "\n");
+        checkIfUserInputIsValidOrNot(testList, false, "6");
+        checkIfUserInputIsValidOrNot(testList, false, "0");
+        checkIfUserInputIsValidOrNot(testList, false, "abc");
+    }
+
+    private <T> void checkIfUserInputIsValidOrNot(List<T> list, boolean shouldBeValid, String userInput) {
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        try {
+            int returnedInput = UserInput.chooseFromList(list);
+
+            if (shouldBeValid && returnedInput == Integer.parseInt(userInput)) {
+                assertTrue(true);
+            } else if (shouldBeValid && returnedInput != Integer.parseInt(userInput)) {
                 fail("User input should be valid and is returned as a valid input, but is not returned correctly");
             } else {
                 fail("User input should not be valid but is returned as a valid input");
