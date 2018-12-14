7245c656822496df8c2332d31cfc0a885186df4e
Ida <ida.fagerlund@hotmail.com>
2018-12-14 10:58:49 +0100

    Remove comment from menu

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,10 @@
 package main;
 
-
 import userinteraction.PhoneBookMenus;
 
 public class Main {
 
     public static void main(String[] args) {
-        //create database in manager instead
         PhoneBookMenus.newMenu().mainMenu();
     }
 }
