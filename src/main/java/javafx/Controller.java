cdb07b1f3410afb081fb1cac70f1941a1e5ff4bc
Ida <ida.fagerlund@hotmail.com>
2018-12-17 13:47:49 +0100

    minor changes

--- src/main/java/javafx/Controller.java
+++ src/main/java/javafx/Controller.java
@@ -1,6 +1,19 @@
 package javafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+
 public class Controller {
+
+    @FXML
+    TableView tableView;
+
+
+    public void something(){
+
+    }
+
 
     public void setStage(){
 
