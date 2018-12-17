f97c0cf05e4aedafeb7cf2eed3affdc5428c4677
DaniellaNoren <43783667+DaniellaNoren@users.noreply.github.com>
2018-12-17 15:26:49 +0100

    Add comments, fix ArrayToObservable (#49)
    * Create converter to observablearraylist
    
    * Finish converter to observable and contact-class
    
    * Add comments, remove some clutter from toContactObservable in ArrayToObservable

--- src/main/java/conversion/ArrayToObservable.java
+++ src/main/java/conversion/ArrayToObservable.java
@@ -2,21 +2,33 @@ package conversion;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import userinteraction.UserOutput;
 
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that converts a List from SelectContact-methods
+ * and creates Contact-instances and an ObservableList for JavaFX
+ * with all the Contacts
+ *
+ * @author Daniella Norén
+ * @version 1 Build 2018
+ */
 public class ArrayToObservable {
 
+    /**
+     * Static method that takes a List<String> from
+     * SelectContact, creates a Contact-instance for each
+     * entry in the List and adds that Contact to an ObservableList<Contact>
+     * used for JavaFX
+     *
+     * @param list List<String> from one of the SelectContact-methods for getting data from Contact-database
+     * @return ObservableList containing Contacts from the contact-database
+     */
     public static ObservableList<Contact> toContactObservable(List<String> list){
         ObservableList<Contact> contactList = FXCollections.observableArrayList();
         for(int i = 0; i < list.size(); i++){
-
-            String contact = list.get(i);
-            String[] contacts = contact.split("\t");
+            String[] contacts = list.get(i).split("\t");
             Contact c = new Contact(contacts[0], contacts[1], contacts[2]);
-            System.out.println(c.getContactID()+" "+c.getName()+" "+c.getNumber());
         }
 
         return contactList;
