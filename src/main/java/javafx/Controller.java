d5e18f0dd1dab8dce1123cd0a37abb75b44165f3
Ida <ida.fagerlund@hotmail.com>
2018-12-18 13:48:13 +0100

    Clean up current code.

--- src/main/java/javafx/Controller.java
+++ src/main/java/javafx/Controller.java
@@ -10,7 +10,10 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * This class manages the communication between the FX version of this program and the database.
+ * @author Ida
+ */
 public class Controller {
 
     @FXML TableView<Contact> tableView;
@@ -30,90 +33,19 @@ public class Controller {
     @FXML Label numberLabel;
 
 
-
-
-    public static Database db = new Database("my_contacts.db");
+    private Database db = new Database("my_contacts.db");
     private Contact currentContact;
     private Boolean confirmUpdate = false;
 
     private ObservableList<Contact> observableContacts;
-    public static List<Contact> searchBackup = new ArrayList<>();
+    public static List<Contact> storedContacts = new ArrayList<>();
 
-    public void search(){
-
-        for(Contact c: searchBackup){
-            db.getAddContact().addContact(c.getName(), c.getNumber());
-        }
-        searchBackup.clear();
-        updateList();
-
-
-        List<Contact> nameMatch = ArrayToObservable.toContactObservable(db.getSelectContact().selectNameContact(searchField.getText()));
-        List<Contact> numberMatch = ArrayToObservable.toContactObservable(db.getSelectContact().selectNumberContact(searchField.getText()));
-
-        for(Contact c: observableContacts){
-            boolean b = false;
-            for(Contact d: nameMatch) {
-                if (c.getId().equals(d.getId()))
-                    b = true;
-            }
-            for(Contact d: numberMatch) {
-                if (c.getId().equals(d.getId()))
-                    b = true;
-            }
-            if(!b){
-                searchBackup.add(new Contact(c.getId(), c.getName(), c.getNumber()));
-                db.getRemoveContact().removeContact(Integer.parseInt(c.getId()));
-            }
-
-        }
-        updateList();
-    }
-
-
-    public void confirm(){
-
-
-        if(nameField.getText().matches("^[a-zA-Z]{1,30}$") && numberField.getText().matches("^[0-9]{1,20}$"))
-            if(currentContact != null && confirmUpdate)
-                db.getRemoveContact().removeContact(Integer.parseInt(currentContact.getId()));
-        db.getAddContact().addContact(nameField.getText(), numberField.getText());
-
-        searchField.setText("");
-        search();
-        updateList();
-        viewTextFields(false);
-
-    }
-
-    private void viewTextFields(boolean b){
-        nameField.setVisible(b);
-        numberField.setVisible(b);
-        nameLabel.setVisible(b);
-        numberLabel.setVisible(b);
-        confirmButton.setVisible(b);
-    }
 
     public void addContact(){
         confirmUpdate = false;
         nameField.setText("");
         numberField.setText("");
         viewTextFields(true);
-    }
-
-    public void tableViewClicked(){
-        if(tableView.getSelectionModel().getSelectedItem() != null){
-            currentContact = tableView.getSelectionModel().getSelectedItem();
-            if(confirmUpdate){
-                nameField.setText(currentContact.getName());
-                numberField.setText(currentContact.getNumber());
-            }
-        }
-    }
-
-    private void updateList(){
-        observableContacts = ArrayToObservable.toContactObservable(db.getSelectContact().selectAllContact());
-        tableView.setItems(observableContacts);
     }
 
     public void updateContact(){
@@ -126,28 +58,92 @@ public class Controller {
     }
 
     public void deleteContact(){
-
-
         if(currentContact != null){
             db.getRemoveContact().removeContact(Integer.parseInt(currentContact.getId()));
             updateList();
         }
     }
 
+    public void confirm(){
+        if(nameField.getText().matches("^[a-zA-Z]{1,30}$") && numberField.getText().matches("^[0-9]{1,20}$"))
+            if(currentContact != null && confirmUpdate)
+                db.getRemoveContact().removeContact(Integer.parseInt(currentContact.getId()));
+        db.getAddContact().addContact(nameField.getText(), numberField.getText());
 
-    public void setStage(){
+        searchField.setText("");
+        search();
+        updateList();
+        viewTextFields(false);
+    }
 
+
+    public void tableViewClicked(){
+        if(tableView.getSelectionModel().getSelectedItem() != null){
+            currentContact = tableView.getSelectionModel().getSelectedItem();
+            if(confirmUpdate){
+                nameField.setText(currentContact.getName());
+                numberField.setText(currentContact.getNumber());
+            }
+        }
+    }
+
+    private void viewTextFields(boolean b){
+        nameField.setVisible(b);
+        numberField.setVisible(b);
+        nameLabel.setVisible(b);
+        numberLabel.setVisible(b);
+        confirmButton.setVisible(b);
+    }
+
+    private void updateList(){
+        observableContacts = ArrayToObservable.toContactObservable(db.getSelectContact().selectAllContact());
+        tableView.setItems(observableContacts);
+    }
+
+
+    public void search(){
+
+        for(Contact c: storedContacts){
+            db.getAddContact().addContact(c.getName(), c.getNumber());
+        }
+        storedContacts.clear();
+        updateList();
+
+        List<Contact> nameMatch = ArrayToObservable.toContactObservable(db.getSelectContact().
+                selectNameContact(searchField.getText()));
+        List<Contact> numberMatch = ArrayToObservable.toContactObservable(db.getSelectContact().
+                selectNumberContact(searchField.getText()));
+
+        for(Contact c: observableContacts){
+            boolean b = false;
+            for(Contact d: nameMatch) {
+                if (c.getId().equals(d.getId()))
+                    b = true;
+            }
+            for(Contact d: numberMatch) {
+                if (c.getId().equals(d.getId()))
+                    b = true;
+            }
+            if(!b){
+                storedContacts.add(new Contact(c.getId(), c.getName(), c.getNumber()));
+                db.getRemoveContact().removeContact(Integer.parseInt(c.getId()));
+            }
+
+        }
+        updateList();
     }
 
 
     public void init(){
         viewTextFields(false);
-
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         numberColumn.setCellValueFactory(new PropertyValueFactory<>("number"));
-
-
-
         updateList();
     }
+
+    public void setStage(){
+    }
+
+
+
 }
