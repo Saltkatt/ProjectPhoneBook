5416a4ae33df11c0778e8db5fd26d5ffbc66b14f
Elin <elin.sexton@iths.se>
2018-12-19 10:51:05 +0100

    Adjustment to javaDoc

--- src/main/java/database/RemoveContact.java
+++ src/main/java/database/RemoveContact.java
@@ -6,21 +6,30 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
- * This class removes a row from the database using the contact_id.
+ * This class removes a contact from the database using the contact_id.
+ * 
  * @author Elin Sexton
  */
 
 public class RemoveContact {
-    /**
-     * Deletes an existing contact from the database.
-     * @param contact_id of the contact.
-     */
 
     private String fileName;
+
+    /**
+     * Constructor allowing connection to Database.
+     *
+     * @param fileName of the database.
+     */
 
     public RemoveContact(String fileName){
         this.fileName = fileName;
     }
+
+    /**
+     * Deletes an existing contact from the database via SQL-statement.
+     *
+     * @param contact_id of the contact.
+     */
 
     public void removeContact(int contact_id){
         //String SQL-statement to delete contact.
